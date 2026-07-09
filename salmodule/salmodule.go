7dd16ab2931718f2550a867db56b712214e43773
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2026-07-09 10:31:12 -0400

    remove salmodule validate. clients should use top level validate

--- salmodule/salmodule.go
+++ salmodule/salmodule.go
@@ -6,7 +6,6 @@ type SalModuleCmd struct {
 	// Ontology is needed so that the sal cli itself is a sal module
 	Ontology *ontologyCmd `arg:"subcommand:ontology" help:"Print the ontology of the sal cli itself"`
 	Run      *runCmd      `arg:"subcommand:run" help:"Run a sal project"`
-	Validate *validateCmd `arg:"subcommand:validate" help:"Validate a sal project"`
 }
 
 func Run(cmd *SalModuleCmd) error {
@@ -15,8 +14,6 @@ func Run(cmd *SalModuleCmd) error {
 		return cmd.Ontology.Run()
 	case cmd.Run != nil:
 		return cmd.Run.Run()
-	case cmd.Validate != nil:
-		return cmd.Validate.Run()
 	default:
 		return fmt.Errorf("salmodule must be ran with a subcommand")
 	}
--- salmodule/validate.go
+++
@@ -1,9 +0,0 @@
-package salmodule
-
-import "fmt"
-
-type validateCmd struct{}
-
-func (cmd *validateCmd) Run() error {
-	return fmt.Errorf("salmodule validate is not yet implemented")
-}
