ee7fff1af1caed9e97435a9c3c93075ec2379b3b
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2026-07-30 08:19:16 -0400

    Add sal module spec to the docs page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,38 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightSiteGraph from 'starlight-site-graph'
-import { fileURLToPath } from 'node:url';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightSiteGraph from "starlight-site-graph";
+import { fileURLToPath } from "node:url";
+import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cgs-earth.github.io',
-	base: '/sal',
-	vite: {
-		resolve: {
-			alias: {
-				micromatch: fileURLToPath(new URL('./src/shims/micromatch.js', import.meta.url)),
-			},
-		},
-		optimizeDeps: {
-			exclude: ['micromatch'],
-		},
-	},
-	integrations: [
-		starlight({
-			plugins: [starlightSiteGraph()],
-			title: 'SAL Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cgs-earth/sal' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [{ autogenerate: { directory: 'guides' } }],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  site: "https://cgs-earth.github.io",
+  base: "/sal",
+  vite: {
+    resolve: {
+      alias: {
+        micromatch: fileURLToPath(
+          new URL("./src/shims/micromatch.js", import.meta.url),
+        ),
+      },
+    },
+    optimizeDeps: {
+      exclude: ["micromatch"],
+    },
+  },
+  integrations: [
+    mermaid({
+      theme: "forest",
+      autoTheme: true,
+    }),
+    starlight({
+      plugins: [starlightSiteGraph()],
+      title: "SAL Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/cgs-earth/sal",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ autogenerate: { directory: "guides" } }],
+        },
+        {
+          label: "Reference",
+          items: [{ autogenerate: { directory: "reference" } }],
+        },
+      ],
+    }),
+  ],
 });
